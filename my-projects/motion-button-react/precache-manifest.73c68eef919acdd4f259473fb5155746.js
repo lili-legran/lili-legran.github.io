42d57b68a7d5f71bac86f59148701ef2abf5ec3d
Lili Legran <lili_akhunyanova@icloud.com>
2020-02-11 00:50:15 +0300

    feat: motion button (React)

---
+++ my-projects/motion-button-react/precache-manifest.73c68eef919acdd4f259473fb5155746.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe72ad01037cb84127f5a12551a09dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "880ac345204aad1aa9d5",
+    "url": "/static/css/main.55c349d6.chunk.css"
+  },
+  {
+    "revision": "f24f957797d1321c707d",
+    "url": "/static/js/2.38aa4268.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.38aa4268.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "880ac345204aad1aa9d5",
+    "url": "/static/js/main.3d9238cf.chunk.js"
+  },
+  {
+    "revision": "0001a64296f9b2db4426",
+    "url": "/static/js/runtime-main.11bf17dc.js"
+  }
+]);
