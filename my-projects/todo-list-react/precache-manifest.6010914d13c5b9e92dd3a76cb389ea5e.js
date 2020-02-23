a59d27786da3f369aa1561b3f0f57d3acffa5999
Lili Legran <lili_akhunyanova@icloud.com>
2020-02-23 19:42:54 +0300

    feat:  Todo List using React

---
+++ my-projects/todo-list-react/precache-manifest.6010914d13c5b9e92dd3a76cb389ea5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3030b9b39d534752e462487ca3cc0c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e88d410f8d1bd9fe61e2",
+    "url": "/static/css/main.94f6e4a6.chunk.css"
+  },
+  {
+    "revision": "947f1057651c61057ad9",
+    "url": "/static/js/2.9d7d2c6b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.9d7d2c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e88d410f8d1bd9fe61e2",
+    "url": "/static/js/main.48711a85.chunk.js"
+  },
+  {
+    "revision": "84ef8d87206aef7a25d0",
+    "url": "/static/js/runtime-main.2bc62508.js"
+  }
+]);
